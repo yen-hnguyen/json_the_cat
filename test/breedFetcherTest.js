0048e8ef5e427d9dba59c5165217019a9852e719
Yen <yen.hnguyen17@gmail.com>
2021-11-19 03:37:27 +0000

    Added mochai & chai testing

---
+++ test/breedFetcherTest.js
@@ -0,0 +1,30 @@
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Chihuahua', (err, desc) => {
+      // we expect error for this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = "Chihuahua is not found";
+      
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+});
