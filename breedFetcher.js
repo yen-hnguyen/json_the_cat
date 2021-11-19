2c8a7f082af82183a4626ff46116ce22cd75f7ef
Yen <yen.hnguyen17@gmail.com>
2021-11-19 03:35:35 +0000

    Refactoring code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,35 @@
 const request = require('request');
 
-let breed = process.argv.splice(2);
-let url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+// request('https://api.thebatapi.com/v1/breeds/search?q=sib', (error, response, body) => {
+//   if (error) {
+//     return console.log(error);
+//   }
 
-request(url, (error, response, body) => {
-  if (error) {
-    return console.log(error.message);
-  }
+//   const data = JSON.parse(body);
 
-  const data = JSON.parse(body);
+//   if (data.length === 0) {
+//     return console.log(`Sib is not found`);
+//   }
+//   console.log(data[0].description);
+// });
 
-  if (data.length === 0) {
-    return console.log(`${breed} is not found`);
-  }
-  console.log(data[0].description);
-});
+
+const fetchBreedDescription = function(breedName, callback) {
+  let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error);
+    }
+
+    const data = JSON.parse(body);
+
+    if (data.length === 0) {
+      callback(null, `${breedName} is not found`);
+    } else {
+      callback(null, data[0].description);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
